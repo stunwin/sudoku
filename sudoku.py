f936f1be7abb8490942d2efe57274b1dbca097c9
Steve Unwin <steve.unwin@gmail.com>
2024-05-04 14:53:57 -0700

    logic is done

--- sudoku.py
+++ sudoku.py
@@ -3,26 +3,36 @@ def checker(arr, x, y):
     ruleout = []
     sub_x, sub_y = x // 3, y // 3
 #check row
-    for n in range(8):
+    for n in range(9):
         num = arr[x][n]
         if isinstance(num, int) and num not in ruleout:
             ruleout.append(num)
+            #print("rownum")
+            #print(num)
+            
 #check column
-    for n in range(8):
-        num = arr[n][x]
+    for n in range(9):
+        num = arr[n][y]
         if isinstance(num, int) and num not in ruleout:
             ruleout.append(num)
+            #print("colnum")
+            #print(num)
 #check subsection
     for row in range(sub_x * 3, (sub_x + 1) * 3):
         for col in range(sub_y * 3, (sub_y + 1) * 3):
-            if isinstance(arr[row][col], int) and num not in ruleout:
+            #print("x:" + str(row) + " y:" + str(col))
+            num = arr[row][col]
+            if isinstance(num, int) and num not in ruleout:
                 ruleout.append(num)
+                #print("chunknum")
+                #print(num)
         options = set(possible)-set(ruleout)
     if len(options) == 1:
         arr[x][y] = list(options)[0]
-        print("got one!" + str(x) +str(y))
-        print(ruleout)
-        print(options)
+        #print("got one!" + str(x) +str(y))
+        #print(str(sub_x) + " " + str(sub_y))
+        #print(ruleout)
+        #print(options)
 
 
 def render_board(arr):
@@ -37,15 +47,15 @@ def render_board(arr):
 
 
 # arr = [["."]*9 for i in range(9)]
-arr = [[5, 3, ".", ".", 7, ".", ".", ".", "."], 
- [6, ".", ".", 1, 9, 5, ".", ".", "."], 
- [".", 9, 8, ".", ".", ".", ".", 6, "."], 
- [8, ".", ".", ".", 6, ".", ".", ".", 3], 
- [4, ".", ".", 8, ".", 3, ".", ".", 1], 
- [7, ".", ".", ".", 2, ".", ".", ".", 6], 
- [".", 6, ".", ".", ".", ".", 2, 8, "."], 
- [".", ".", ".", 4, 1, 9, ".", ".", 5], 
- [".", ".", ".", ".", 8, ".", ".", 7, 9]]
+arr = [[4, 9, 2, 6, ".", ".", ".", ".", 1], 
+ [".", ".", ".", ".", ".", ".", 7, ".", 8], 
+ [".", ".", 6, 3, 1, 5, ".", ".", "."], 
+ [6, ".", 5, 2, 3, ".", 8, ".", "."], 
+ [1, ".", 4, ".", ".", ".", ".", 7, 2], 
+ [".", ".", ".", 7, 5, 1, ".", 3, "."], 
+ [8, ".", ".", 1, 6, 7, ".", 2, "."], 
+ [2, 1, ".", 5, ".", ".", ".", 4, "."], 
+ [".", ".", 7, ".", 2, ".", 1, 8, 3]]
  
 possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -53,18 +63,18 @@ print("input!")
 
 render_board(arr)
 
-#checker(arr, 0, 2)
+#checker(arr, 4, 1)
 
 #print("output!")
 
 #render_board(arr)
 
 #TODO math go here
-
-for x, row in enumerate(arr):
-    for y, num in enumerate(row):
-        if arr[x][y] == ".":
-            checker(arr, x, y)
+while any("." in i for i in arr):
+    for x, row in enumerate(arr):
+        for y, num in enumerate(row):
+            if arr[x][y] == ".":
+                checker(arr, x, y)
 
 
 print("output!")
